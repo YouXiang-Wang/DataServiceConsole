7abf0358ec034f0454639d755ac005b278b8ac02
Birdflying1005 <youxiang.w@163.com>
2014-09-23 10:44:38 +0800

    s

--- DataToolsConsole/js/ibm/datastudio/pmrs/views/PmrObjectModel.js
+++ DataToolsConsole/js/ibm/datastudio/pmrs/views/PmrObjectModel.js
@@ -1,39 +1,63 @@
 define([
 		"dojo/_base/declare",
-		"dojo/_base/lang",
-		"dojo/_base/connect",
-		"dojo/_base/array",
-		"dojo/dom-construct",
-		
-		"dcc/datatools/common/BaseModel",
-		"dcc/datatools/context/AppContext",
-		"dcc/datatools/helper/DataConvertHelper",
-		"./PmrObjectCons"
-		
-	], function(declare, lang, connect, array, domConstruct, 
-			BaseModel, AppContext,dataConvertHelper, PmrObjectCons ) {
-	
-	
-	return declare([BaseModel], {
-
-		constructor: function(){
-			this.dataGridStore = null;
-		},	
-
-		dataWrapperForViewer: function(response){
-			//var result = response.result; //.result;
-			var result = {
-					identifier: "pmrNumber",
-					items: response.result
-			}
-			
-			var memoryStore = dataConvertHelper.createMemoryStoreFromArray(result);
-			this.dataGridStore = memoryStore;
-			return memoryStore;
-		},
-		
-		getFilterStore: function(options){
-			return dataConvertHelper.createMemoryStoreFromArray(options);
+		"dojo/_base/lang"
+	], function(declare, lang) {
+		/*
+		var column = function(label, field){
+			return {label: label, field: field}
 		}
-	});
-});
+		*/
+	
+		var column = function(label, field){
+			return {label: label, id: field}
+		}
+	
+		var tableProperties = 
+			[
+		      column('PMR Number', 'pmrNumber'),
+		      column('Component', 'l3Group'),
+		      column('L3 Owner', 'l3Owner'),
+		      column('Customer', 'customer'),
+		      column('Version', 'version'),
+		      column('Severity', 'severity'),
+		      column('Priority', 'priority'),
+		      column('PMR Status', 'pmrStatus'),
+		      column('APAR', 'apar'),
+		      column('Request Date', 'l3RequestDate'),
+		      column('Close Date', 'l2CloseDate')
+		      
+		    ];
+		
+		/*
+		var tableProperties = 
+			[
+		      column('PMR Number', 'pmrNumber'),
+		      column('Component', 'l3Group'),
+		      column('L3 Owner', 'l3Owner'),
+		      column('Customer', 'customer'),
+		      column('Version', 'openAgainst'),
+		      column('Severity', 'severity'),
+		      column('Priority', 'priority'),
+		      column('PMR Status', 'pmrStatus'),
+		      column('Request Date', 'l3RequestDate'),
+		      column('Close Date', 'l3CloseDate'),
+		      column('APAR', 'apar')
+		    ];
+		*/
+		
+		
+		var events = {
+				LOAD_DATA_READY: 'loadDataReady'
+		}
+		
+		var requests = {
+				REQ_DATA_OBJECT_MOCKUP: '/data/tables.json'
+		}
+		
+		return {
+			tableProperties: tableProperties,
+			events: events,
+			requests: requests
+		}
+	}
+);
