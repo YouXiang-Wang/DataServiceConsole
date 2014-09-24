f49fca111013575e2dd38566aea5e49a22ebc2ee
Birdflying1005 <youxiang.w@163.com>
2014-09-24 09:39:55 +0800

    s

--- DataToolsConsole/js/ibm/datastudio/pmrs/views/PmrObjectView.js
+++ DataToolsConsole/js/ibm/datastudio/pmrs/views/PmrObjectView.js
@@ -96,7 +96,7 @@ define([
 								get: function (rowData) {
 							    	var value = rowData.customer;
 							    	if(value!=null && value!='') {
-							            return value.substr(16);
+							            return value;
 							    	}
 							    	return "";
 						        }},
@@ -121,11 +121,19 @@ define([
 								get: function (rowData) {
 							    	var value = rowData.l3RequestDate;
 							    	if(value!=null && value!='') {
-							    		return value.substr(0, 10);
+							    		return value;
 							    	}
 							    	return "";
 						        }},
-					l3CloseDate: { label: "Close Date"},
+					l3CloseDate: { label: "Close Date",
+								width: 120,
+								get: function (rowData) {
+							    	var value = rowData.l3CloseDate;
+							    	if(value!=null && value!='') {
+							    		return value;
+							    	}
+							    	return "";
+						        }},
 					apar: { label: "APAR", width: 80},
 					comments: { label: "Comments"},
 				};
@@ -290,7 +298,7 @@ define([
 					var _command = {
 						moduleID: "GET_PMR",
 						displayTitle: _pmrNumber,
-						contextPath: "http://localhost:3200",
+						contextPath: "http://9.123.149.188:3200", //"http://localhost:3200",
 						args: {type: 1,
 						  	pmrNumber: _pmrNumber
 						},
@@ -302,10 +310,10 @@ define([
 					
 					var commandHandler = new CommandHandler({
 						  args: {type: 1,
-							  	pmrNumber: '01981,442,000', //_pmrNumber
+							  	pmrNumber: _pmrNumber
 							  },
 						  context: {data: _row},
-						  commandText: '01981,442,000', //_pmrNumber,
+						  commandText: _pmrNumber, //'01981,442,000'
 						  commandDef: _command
 					  });
 					
