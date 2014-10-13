7a7449e4dfb4318c3e1d9f81091397459d219ccf
Birdflying1005 <youxiang.w@163.com>
2014-10-13 10:06:40 +0800

    s

---
+++ DataToolsConsole/js/ibm/datastudio/constants/constants.js
@@ -0,0 +1,16 @@
+define([
+		"dojo/_base/declare",
+		"dojo/_base/lang",
+		'dojo/topic'
+	], function(declare, lang, topic) {
+		
+		return {
+			L3GROUP_ADMINISTRATION: 'Data Studio Administrator',
+			
+			L3GROUP_DEVELOPER: 'Optim Development Studio /  Data Studio (Developer)',
+			
+			L3GROUP_CORE: 'Optim Data Studio Core Development',
+			
+		}
+	}
+);
